e0a5c0faf34f19ffe8dbdc5ce5490429ba2a4f9f
bswck <bswck.dev@gmail.com>
2024-02-02 06:13:57 +0100

    Build from "sources"

--- mktopics.bash
+++ mktopics.bash
@@ -10,8 +10,13 @@
 
 set -eEuo pipefail
 
-__OK="\033[0;32m✔\033[0m"
-__ERR="\033[0;31m✖\033[0m"
+: "${MKTOPICS_SOURCES_DIR:=./sources}"
+
+MKTOPICS_LEVELS=(
+    "easy"
+    "medium"
+    "hard"
+)
 
 mktopic() {
     local d dl lv lvs kf="${MKTOPICS_KEEPFILE:=.gitkeep}"
@@ -25,151 +30,30 @@ mktopic() {
     echo "$d"
 }
 
-onerror() {
-    echo -e "${__ERR} An error occurred ($0:$1)" >&2
-}
-
-trap 'onerror $LINENO' ERR
-
-MKTOPICS_LEVELS=(
-    "easy"
-    "medium"
-    "hard"
-)
-
-MKTOPICS_SOURCES=(
-    "leetcode"
-    "codeforces"
-)
-
-MKTOPICS_LEETCODE_TOPICS=(
-    "Array"
-    "String"
-    "Hash Table"
-    "Dynamic Programming"
-    "Math"
-    "Sorting"
-    "Greedy"
-    "Depth-First Search"
-    "Binary Search"
-    "Database"
-    "Breadth-First Search"
-    "Tree"
-    "Matrix"
-    "Two Pointers"
-    "Bit Manipulation"
-    "Binary Tree"
-    "Heap (Priority Queue)"
-    "Stack"
-    "Prefix Sum"
-    "Graph"
-    "Simulation"
-    "Design"
-    "Counting"
-    "Sliding Window"
-    "Backtracking"
-    "Union Find"
-    "Linked List"
-    "Enumeration"
-    "Ordered Set"
-    "Monotonic Stack"
-    "Number Theory"
-    "Trie"
-    "Recursion"
-    "Divide and Conquer"
-    "Bitmask"
-    "Queue"
-    "Binary Search Tree"
-    "Segment Tree"
-    "Memoization"
-    "Binary Indexed Tree"
-    "Geometry"
-    "Topological Sort"
-    "Combinatorics"
-    "Hash Function"
-    "Shortest Path"
-    "Game Theory"
-    "String Matching"
-    "Data Stream"
-    "Interactive"
-    "Rolling Hash"
-    "Brainteaser"
-    "Monotonic Queue"
-    "Randomized"
-    "Merge Sort"
-    "Iterator"
-    "Concurrency"
-    "Doubly-Linked List"
-    "Probability and Statistics"
-    "Quickselect"
-    "Bucket Sort"
-    "Suffix Array"
-    "Minimum Spanning Tree"
-    "Counting Sort"
-    "Shell"
-    "Line Sweep"
-    "Reservoir Sampling"
-    "Strongly Connected Component"
-    "Eulerian Circuit"
-    "Radix Sort"
-)
-
-MKTOPICS_CODEFORCES_TOPICS=(
-    "2-sat"
-    "binary search"
-    "bitmasks"
-    "brute force"
-    "chinese remainder theorem"
-    "combinatorics"
-    "constructive algorithms"
-    "data structures"
-    "dfs and similar"
-    "divide and conquer"
-    "dp"
-    "dsu"
-    "expression parsing"
-    "fft"
-    "flows"
-    "games"
-    "geometry"
-    "graph matchings"
-    "graphs"
-    "greedy"
-    "hashing"
-    "implementation"
-    "interactive"
-    "math"
-    "matrices"
-    "meet-in-the-middle"
-    "number theory"
-    "probabilities"
-    "schedules"
-    "shortest paths"
-    "sortings"
-    "string suffix structures"
-    "strings"
-    "ternary search"
-    "trees"
-    "two pointers"
-)
-
 entrypoint() {
-    local topic source dest_info source_info vcs_aware=0
+    local topic source source_files dest_info source_info vcs_aware=0
     test "${1:-}" = "--vcs-aware" && vcs_aware=1
-    for source in "${MKTOPICS_SOURCES[@]}"
+    readarray -t source_files <<< "$(find "$MKTOPICS_SOURCES_DIR" -type f -name "*.txt")"
+    echo -e "\033[0;32m✔\033[0m Found \033[0;34m${#source_files[@]}\033[0m source(s): ${source_files[*]}"
+    for source_file in "${source_files[@]}"
     do
-        declare -n source_ptr
-        source_ptr="MKTOPICS_$(echo "$source" | tr '[:lower:]' '[:upper:]')_TOPICS"
-        for topic in "${source_ptr[@]}"
+        source=$(basename "$source_file" .txt)
+        readarray -t topics < "$source_file"
+        for topic in "${topics[@]}"
         do
             created_topic=$(mktopic "$source" "$topic")
             [ $vcs_aware -eq 1 ] && git add --intent-to-add "$created_topic"
             dest_info="\033[0;36m→ $created_topic\033[0m"
             source_info="\033[0;34m$source\033[0m"
-            echo -e "${__OK} ($source_info) Created topic \"$topic\" $dest_info"
+            echo -e "\033[0;32m✔\033[0m ($source_info) Created topic \"$topic\" $dest_info"
         done
-        unset -n source_ptr
     done
 }
+
+onerror() {
+    echo -e "\033[0;31m✖\033[0m An error occurred ($0:$1)" >&2
+}
+
+trap 'onerror $LINENO' ERR
 
 entrypoint "$@"
